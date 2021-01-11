1d4e6c816301c051445874c27b855472e6b4ac6e
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-01-11 00:08:22 -0300

    Add Main Form, Breadcrumb and list group

--- Admin/index.php
+++ Admin/index.php
@@ -116,11 +116,17 @@
 
                         <ul class="list-group">
 
-                            <li class="list-group-item">Cras justo odio</li>
-                            <li class="list-group-item">Dapibus ac facilisis in</li>
-                            <li class="list-group-item">Morbi leo risus</li>
-                            <li class="list-group-item">Porta ac consectetur ac</li>
-                            <li class="list-group-item">Vestibulum at eros</li>
+                            <li class="list-group-item list-group-title"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door-fill list-group-home" viewBox="0 0 16 16">
+                                <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
+                            </svg>Home</li>
+
+                            <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill list-group-icon" viewBox="0 0 16 16">
+                                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                            </svg>Extra</li>
+
+                            <li class="list-group-item"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill list-group-icon" viewBox="0 0 16 16">
+                                <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                        </svg>Team</li>
 
                         </ul><!--list-group-->
 
@@ -128,7 +134,45 @@
 
                     <div class="col-md-9">
 
+                        <div class="panel panel-default">
 
+                            <div class="panel-heading">
+
+                                <h3>Extras</h3>
+
+                            </div><!--panel-heading-->
+
+                            <div class="panel-body">
+
+                                <form action="index.php">
+                                
+                                    <div class="form-group">
+
+                                        <label for="email">Email address:</label>
+                                        <input type="email" class="form-control" id="email">
+
+                                    </div><!--form-group-->
+
+                                    <div class="form-group">
+
+                                        <label for="pwd">Password:</label>
+                                        <input type="password" class="form-control" id="pwd">
+
+                                    </div><!--form-group-->
+
+                                    <div class="checkbox">
+
+                                        <label><input type="checkbox"> Remember me</label>
+
+                                    </div><!--checkboc-->
+
+                                    <button type="submit" class="btn btn-outline-dark">Submit</button>
+
+                                </form>
+
+                            </div><!--panel-body-->
+
+                        </div><!--panel-default-->
 
                     </div><!--col-md-9-->
 
