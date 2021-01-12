5523ecdcfb53d30acdd8558965372e7ff8e32d2a
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-01-12 13:47:15 -0300

    Add reative header

--- Admin/Scripts/main.js
+++ Admin/Scripts/main.js
@@ -1,14 +1,40 @@
 $(function () {
     function Click() {
-        $('.navbar-menu a, .list-group-item a').click(() => {
-            var attribute = $(this).attr()
+        $('.item-home').addClass('active')
 
-            console.log(attribute)
+        function setActivity(target) { 
+            $(target).click(() => {
+                if (!$(target).hasClass('active')) {
+                    if ($('.nav-item, .list-group-item').hasClass('active')) {
+                        $('.nav-item, .list-group-item').removeClass('active')
+    
+                        $(target).addClass('active')
+                    }
+                    else {
+                        $(target).addClass('active')
+                    }
+                }
 
-            // don't refresh the page.
-            return false
+                return false
+            })
+        }
+
+        setActivity('.item-home')
+        setActivity('.item-addMember')
+        setActivity('.item-memberList')
+        setActivity('.item-extra')
+    }
+
+    function ScrollToItem(target1, target2) { 
+        $(target1, target2).click(() => {
+            var ref = $(target1, target2).attr('class')
+
+            // var offset = $(ref).offset().top
+
+            alert(ref)
         })
     }
 
     Click()
+    ScrollToItem('.nav-item', '.list-group-item')
 })
