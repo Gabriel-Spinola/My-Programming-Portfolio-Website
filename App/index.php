eb2f2d0755649ad9b80e169df77f03e644c27486
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-01-12 20:58:32 -0300

    Translate

--- App/index.php
+++ App/index.php
@@ -21,7 +21,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link href="css/style.css" rel="stylesheet">
 
-    <title>Projeto Bootstrap</title>
+    <title>Bootstrap Project</title>
 
 </head>
 
@@ -46,8 +46,8 @@
                     <ul class="navbar-nav me-auto mb-2 mb-md-0">
 
                         <li class="nav-item active"> <a class="nav-link" aria-current="page" href="#">Home</a> </li>
-                        <li class="nav-item"> <a class="nav-link" href="sobre">Sobre</a> </li>
-                        <li class="nav-item"> <a class="nav-link " href="contato" tabindex="-1">Contato</a> </li>
+                        <li class="nav-item"> <a class="nav-link" href="about">About</a> </li>
+                        <li class="nav-item"> <a class="nav-link " href="contact" tabindex="-1">contact</a> </li>
 
                     </ul><!--navbar-nav--->
 
@@ -65,7 +65,7 @@
 
             <div class="overlay"></div>
 
-            <div class="container chamada-banner">
+            <div class="container main-banner">
 
                 <div class="row">
 
@@ -73,9 +73,9 @@
 
                         <h2>&lt;Danki Code&gt;</h2>
 
-                        <p>Empresa voltada para desenvolvimento web marketing digital</p>
+                        <p>Company focused on digital marketing and web development</p>
 
-                        <a href="">Saiba Mais!</a>
+                        <a href="">Know More!</a>
 
                     </div><!--col-md-12--->
 
@@ -85,7 +85,7 @@
 
         </section><!--banner--->
 
-        <section class="cadastro-lead">
+        <section class="main-register">
 
             <div class="container">
 
@@ -96,7 +96,7 @@
                         <h2>
                             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-check2" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
-                            </svg> Entre na nossa lista
+                            </svg> Join our list
                         </h2>
 
                     </div>
@@ -115,13 +115,13 @@
 
             </div><!--container-->
 
-        </section><!--cadastro-lead-->
+        </section><!--main-register-->
 
-        <section class="diferenciais text-center">
+        <section class="differentials text-center">
 
             <h2>Conheça Nossa Empresa</h2>
 
-            <div class="container diferenciais-container">
+            <div class="container differentials-container">
 
                 <div class="row">
 
@@ -129,11 +129,11 @@
                     
                 </div><!--row-->
 
-            </div><!--container-->
+            </div><!--differentials-container-->
 
-        </section><!--diferenciais-->
+        </section><!--differentials-->
 
-        <section class="depoimento text-center">
+        <section class="testimonial text-center">
 
             <div class="container">
 
@@ -141,7 +141,7 @@
 
                     <div class="col-md-12">
 
-                        <h2>Depoimento</h2>
+                        <h2>testimonial</h2>
 
                         <blockquote>
 
@@ -155,13 +155,13 @@
 
             </div><!--container-->
 
-        </section><!--depoimento-->
+        </section><!--testimonial-->
 
-        <section class="equipe">
+        <section class="team">
 
-            <h2>Equipe</h2>
+            <h2>Team</h2>
 
-            <div class="container equipe-container">
+            <div class="container team-container">
 
                 <div class="row">
 
@@ -181,7 +181,7 @@
 
                         <div class="col-md-6">
 
-                            <div class="equipe-single">
+                            <div class="team-single">
 
                                 <div class="row">
 
@@ -205,7 +205,7 @@
 
                                 </div><!--row-->
                                 
-                            </div><!--equipe-single-->
+                            </div><!--team-single-->
 
                         </div><!--col-md-6-->
 
@@ -213,13 +213,13 @@
                     
                 </div><!--row-->
 
-            </div><!--equipe-container-->
+            </div><!--team-container-->
 
-        </section><!--equipe-->
+        </section><!--team-->
 
         <hr>
 
-        <section class="contato-planos">
+        <section class="contract-plans">
 
             <div class="container">
 
@@ -227,13 +227,13 @@
 
                     <div class="col-md-6">
 
-                        <h2>fale Conosco</h2>
+                        <h2>Contact Us</h2>
 
                         <form method="post">
 
                             <div class="form-group">
 
-                                <label for="nome">nome:</label>
+                                <label for="nome">name:</label>
                                 <input type="text" name="name" class="form-control" id="nome">
 
                             </div><!--form-group-->
@@ -247,8 +247,8 @@
 
                             <div class="form-group">
 
-                                <label for="mensagem">Mensagem:</label>
-                                <textarea class="form-control"></textarea>
+                                <label for="message">Message:</label>
+                                <textarea class="form-control" id="message"></textarea>
 
                             </div><!--form-group-->
 
@@ -260,7 +260,7 @@
 
                     <div class="col-md-6">
 
-                        <h2>Nossos Planos</h2>
+                        <h2>Our Plans</h2>
 
                         <table class="table table-bordered table-light">
 
@@ -268,9 +268,9 @@
 
                                 <tr>
 
-                                    <th>Plano Semanal</th>
-                                    <th>Plano Mensal</th>
-                                    <th>Plano Anual</th>
+                                    <th>Weakly Plan</th>
+                                    <th>Monthly Plan</th>
+                                    <th>Annual Plan</th>
 
                                 </tr>
 
@@ -326,7 +326,7 @@
 
     <footer>
 
-        <p class="text-center">&copy;Todos Direitos Reservados</p>
+        <p class="text-center">All Rights Reserved&copy;</p>
 
     </footer><!--END FOOTER--->
 
